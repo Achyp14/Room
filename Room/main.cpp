8f72186f1b994f1dd5e8c50a9d741e74322807a5
achyp14 <achyp14@gmail.com>
2018-04-11 18:11:56 +0300

    First commit

---
+++ Room/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Room
+//
+//  Created by Andrew Chypurko on 4/11/18.
+//  Copyright © 2018 Andrew Chypurko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
